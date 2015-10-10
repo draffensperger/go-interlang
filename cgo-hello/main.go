a522b8c867580b1bf5e29027a593e49d5d1b2de8
draffensperger <d.raffensperger@gmail.com>
2015-10-10 17:21:15 -0400

    Added cgo hello world

---
+++ cgo-hello/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+/*
+#include <stdio.h>
+int adder(int x, int y) {
+	printf("Hello from C: adding %i + %i in C\n", x, y);
+	return x + y;
+}
+*/
+import "C"
+
+func main() {
+	z := C.adder(10, 20)
+	fmt.Printf("Hello from Go: the result was: %v\n", z)
+}
